7249fb9827240a94a5da95a8e8884e12ace7ef89
benjamin <benjamin.morales0401@gmail.com>
2025-11-04 00:08:46 -0300

    se elimina info.tsx y se deja solo infoButton.tsx y se complemtna con parte de lo que hbaia en info.tsx y se agregan comentarios para comprencion

--- src/components/shared/InfoButton.tsx
+++ src/components/shared/InfoButton.tsx
@@ -1,21 +1,33 @@
 
 
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { InfoList } from "./InfoList";
 import "../../Styles/InfoButton.css";
 
-export const InfoButton = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
+export const InfoButton = () => {
+ const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+
+  /* menu abierto/cerrado */ 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  /* cerrar menu*/ 
   const handleClose = () => {
     setIsOpen(false);
   };
 
-  return (
+  /* ir a navegacion */
+  const handleNavigation = () => {
+    navigate("/navegacion");
+  };
+
+return (
+  <>
+        {/*Boton Informacion*/}
     <div className="info-button-container">
       <button 
         className="info-button"
@@ -23,7 +35,20 @@ export const InfoButton = () => {
       >
         ❗
       </button>
+    </div>
       
+      {/*Boton Navegacion*/}
+    <div className="navigation-button-container">
+        <button 
+          className="navigation-button"
+          onClick={handleNavigation}
+        >
+          +
+        </button>
+    </div>
+
+
+      {/* Menú desplegable */}
       {isOpen && (
         <div className="info-list-overlay" onClick={handleClose}>
           <div className="info-list-wrapper" onClick={(e) => e.stopPropagation()}>
@@ -31,6 +56,6 @@ export const InfoButton = () => {
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
