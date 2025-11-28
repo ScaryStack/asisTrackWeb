5163e6cdf811a4eca0714f8c7910cea331a219d5
benjamin <benjamin.morales0401@gmail.com>
2025-11-28 19:32:44 -0300

    Se añade configuración de alias y proxy en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+   alias: {
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/Styles')
+    } 
+  },
+   server: {
+    proxy: {
+      '/api': {
+        target: 'https://asistrack-apis.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
