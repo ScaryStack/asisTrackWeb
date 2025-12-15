cb5ecc12172983bae95f3b441d940b05c2efbc89
benjamin <benjamin.morales0401@gmail.com>
2025-12-15 00:43:15 -0300

    Se agran diseños y diseño de login con botstrap y css

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,19 @@
+import "../styles/login.css";
 import { LoginForm } from "../components/shared/LoginForm";
-import "../global.css"
 
 export const Login = () => {
-  return <LoginForm />;
+  return (
+    <div className="login-background">
+      <div className="container-fluid min-vh-100">
+        <div className="row min-vh-100 align-items-center justify-content-end">
+
+          {/* COLUMNA DEL LOGIN */}
+          <div className="col-12 col-sm-10 col-md-6 col-lg-4 col-xl-3 me-lg-5">
+            <LoginForm />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
 };
